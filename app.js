ec57243dca3e9831844eb062c26d1c731dc7f61b
Astrian Zheng <git@astrianzheng.cn>
2021-07-26 18:05:32 +0800

    Refresh new announcements

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
+const port = 3000
+
+const Koa = require('koa')
+const app = new Koa()
+const route = require('koa-route')
+const axios = require('axios')
+const schedule = require('node-schedule')
 const func = require('./func')
-async function main (){
-  await func.dbwrite({announcement: 444})
-  console.log(await func.dbread({announcement: 444}))
+const { Telegraf, Telegram } = require('telegraf')
+
+// const tgbotTelegraf = new Telegraf(process.env.ARK_TGBOT)
+const tgbotTelegram = new Telegram(process.env.ARK_TGBOT)
+
+app.use(route.get('/refresh', async ctx => {
+  await refresh()
+  ctx.response.status = 204
+}))
+
+app.listen(port, () => {
+  console.log(`Application is running on port ${port}`)
+})
+
+async function refresh() {
+  console.log('refresh!')
+  let result = await axios.get('https://ak-conf.hypergryph.com/config/prod/announce_meta/IOS/announcement.meta.json')
+  let list = result.data.announceList
+  for (let i in list) {
+    console.log(`The announcement which is processing is ${list[i].announceId}.`)
+    console.log(`Finding announcement on database...`)
+    let res = await func.dbread({announceId: list[i].announceId})
+    if (!res.length) {
+      console.log('New announcement!')
+      console.log(list[i])
+      await tgbotTelegram.sendMessage(
+        process.env.ARK_CHATID,
+        `*新游戏内公告*：[${list[i].title.replace(/[\r\n]/g,"")}](${list[i].webUrl})\n${list[i].group === 'SYSTEM' ? '\\#系统公告' : '\\#活动通知'}`,
+        {
+          parse_mode: 'MarkdownV2',
+          disable_web_page_preview: true
+        }
+      )
+      await func.dbwrite({announceId: list[i].announceId})
+    }
+  }
 }
 
-main()
+async function scheduleTask() {
+  let rule = new schedule.RecurrenceRule()
+  rule.minute = [0, 15, 30, 45]
+
+  let job = schedule.scheduleJob(rule, async () => {
+    await refresh()
+  })
+}
+
+scheduleTask()
--- func/dbwrite.js
+++ func/dbwrite.js
@@ -1,16 +1,15 @@
-const config = require('../config.json')
 const { MongoClient } = require('mongodb')
 // or as an es module:
 // import { MongoClient } from 'mongodb'
 
 // Connection URL
-const url = `mongodb+srv://${config.database.username}:${config.database.password}@cluster0.4yupq.mongodb.net/?retryWrites=true&w=majority`
+const url = `mongodb+srv://${process.env.ARK_DBUSER}:${process.env.ARK_DBPWD}@cluster0.4yupq.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(url)
 
 module.exports = async (data) => {
   // Use connect method to connect to the server
   await client.connect()
-  const db = client.db(config.database.name)
+  const db = client.db(process.env.ARK_DBNAME)
   const collection = db.collection('announcements')
 
   // the following code examples can be pasted here...
